63bc4972b485e30ac0b306763ccd21d2c6509d1b
Elaine Lai <elaineelaii@hotmail.com>
2021-12-17 14:02:40 -0800

    added firebaseAPI file

---
+++ js/firebaseAPI.js
@@ -0,0 +1,16 @@
+//copied from demo 8
+
+// Your web app's Firebase configuration
+var  firebaseConfig = {
+apiKey: "AIzaSyDttK3W9EaSI8QLKAez_f-4g9AXPHC-WhI",
+  authDomain: "nomo-e3a8e.firebaseapp.com",
+  projectId: "nomo-e3a8e",
+  storageBucket: "nomo-e3a8e.appspot.com",
+  messagingSenderId: "271588415710",
+  appId: "1:271588415710:web:740ba21c7fb59e8d078a4f",
+  measurementId: "G-YBMGT82WMF"
+};
+  
+//initializes your firebase project in your local repo
+const app = firebase.initializeApp(firebaseConfig);
+  
