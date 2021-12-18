467baef17a74d19d1121ffd171fdc6e10ab433fb
JungMin Oh <joh67@my.bcit.ca>
2021-12-17 23:09:12 -0800

    fixed styling for third page

--- js/thirdPage.js
+++ js/thirdPage.js
@@ -17,7 +17,7 @@ function loadButtons(){
       document.getElementById("newButtons").appendChild(newQuoteButton);
       var btnChild = document.getElementById("newButtons").children;
       for (var i2 = 0; i2 < btnChild.length; i2++){
-        btnChild[i2].style.display = "block";
+        btnChild[i2].style.display = "inline-block";
       }
       // console.log(quoteList[i]);
     }
