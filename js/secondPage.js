a73c8d482623e1578af8c5f614b57d6cf78c4996
JungMin Oh <joh67@my.bcit.ca>
2021-12-17 21:57:46 -0800

    added first second third fourth page html and css and second page js, jason

--- js/secondPage.js
+++ js/secondPage.js
@@ -2,28 +2,49 @@ function queryDecector() {
     var urlParams = new URLSearchParams(window.location.search);
     if (urlParams.has('business')) {
         document.getElementById("firstButton").innerText = "Coffee Chat";
+        document.getElementById("firstIcon").innerText = "person";
 
         document.getElementById("secondButton").innerText = "Job Offer";
+        document.getElementById("secondIcon").innerText = "work";
 
         document.getElementById("thirdButton").innerText = "Shift";
-
+        document.getElementById("thirdIcon").innerText = "schedule";
+        
         document.getElementById("fourthButton").innerText = "Sick";
+        document.getElementById("fourthIcon").innerText = "sick";
 
         document.getElementById("fifthButton").innerText = "Work Event";
+        document.getElementById("fifthIcon").innerText = "event";
+        
+        document.getElementById("sixthButton").innerText = "Work Event";
+        document.getElementById("sixthIcon").innerText = "event";
 
-        document.getElementById("CategoryHeading").innerText = "BUSINESS";
-        console.log("business works");
+        document.getElementById("CategoryHeading").innerText = "Business";
+        // console.log("business works");
 
         changeHREF("business");
 
     } else if (urlParams.has('personal')) {
         document.getElementById("firstButton").innerText = "Babysit";
+        document.getElementById("firstIcon").innerText = "child_friendly";
+
         document.getElementById("secondButton").innerText = "Date";
-        document.getElementById("thirdButton").innerText = "Family Hangout";
+        document.getElementById("secondIcon").innerText = "favorite";
+
+        document.getElementById("thirdButton").innerText = "Family";
+        document.getElementById("thirdIcon").innerText = "family_restroom";
+
         document.getElementById("fourthButton").innerText = "Self Invitation";
+        document.getElementById("fourthIcon").innerText = "mail";
+
         document.getElementById("fifthButton").innerText = "Hangout";
-        document.getElementById("CategoryHeading").innerText = "PERSONAL";
-        console.log("personal works");
+        document.getElementById("fifthIcon").innerText = "group_add";
+
+        document.getElementById("sixthButton").innerText = "Work Event";
+        document.getElementById("sixthIcon").innerText = "favorite";
+
+        document.getElementById("CategoryHeading").innerText = "Personal";
+        // console.log("personal works");
 
         changeHREF("personal");
     } else {
